1b68878da8b2c1f9f0f2f1cfea250448e67e94c2
tslawek <tomek.slawek@gmail.com>
2009-05-24 15:35:16 +0200

    find_names functionality

---
+++ names.py
@@ -0,0 +1,9 @@
+name1 = """Adam Nowak Nowakowski
+ (ur. 4 maja 1762 w Volkstedt, zm. 28 kwietnia 1816 w Wilnie) - filozof niemiecki."""
+
+name2 = """Jan Kowalski (ur. 1 stycznia 1245 w Polsce) itp itd"""
+
+name3 = """To jest czesc teksut Tomasz Gil (ur. 3 czerwca 1243 w USA) sadasd t i dalsza czesc"""
+
+
+name4 = """Jan Kowalski (ur. 1 stycznia 1245 w Polsce) itp itd oraz Adam Mickiewicz (ur. 5 maja i tak dalej itp itd"""
