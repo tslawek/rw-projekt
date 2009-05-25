fa91fd6997d79bbd49acd5c5c6353a64f6c2e435
Konrad Delong <konryd@gmail.com>
2009-05-25 13:00:38 +0200

    further cleanup/refactor

--- main.py
+++ main.py
@@ -1,77 +1,15 @@
 #ecoding=utf-8
 
-import re,sys
-import corpus
+import sys
 import xml2text
-
-def localesub(pattern, replacement, string):
-    pattern = re.compile(pattern, re.LOCALE)
-    return re.sub(pattern, replacement, string)
-
-def test_strip_info():
-    assert strip_info("[[link]]") == "[[link]]"
-    assert strip_info("[[a:s]]") == ""
-    assert strip_info("[[link]] [[a:s]]") == "[[link]] "
-    assert strip_info("[[a:s]] [[link]]") == " [[link]]"
-
-    assert strip_info("jeden [[link]]") == "jeden [[link]]"
-    assert strip_info("jeden [[link]] dwa") == "jeden [[link]] dwa"
-    assert strip_info("jeden [[link]] [[Kategoria:Polacy]] dwa") == "jeden [[link]]  dwa"
-    assert strip_info("[[Plik:Pierogi frying.jpg|thumb|246px|Pierogi]]") == ""
-    
-
-def strip_info(text):
-    return localesub(r"\[\[[^\[\]]+:[^\[\]]+\]\]", "", text)
-
-def test_strip_links():
-    assert strip_links("[[20]]") == "20"
-    assert strip_links("[[1920|20]]") == "20"
-    assert strip_links("[[Flaga Rzeczypospolitej Polskiej|Flaga Polski]]") == "Flaga Polski"
-    assert strip_links("[[Łotysze|Łotyszów]]") == "Łotyszów"
-    assert strip_links("jeden [[Flaga Rzeczypospolitej Polskiej|Flaga Polski]] dwa") == "jeden Flaga Polski dwa"
-
-def strip_links(text):
-    return localesub(r"\[\[(?:[^\[\]]*\|)?([^\[\]]*)\]\]", r"\1", text)
-
-def test_strip_markup():
-    assert strip_markup("'''asdf'''") == "asdf"
-    assert strip_markup("* asdf") == "asdf"
-    assert strip_markup("jeden {{ adsf \n asdf \n }} dwa") == "jeden  dwa"
-    _in = """* Albin Janusz, ''"Polski ruch narodowy na Łotwie w latach 1919-40"'', [[Wrocław]] [[1993]], ISBN 83-229-0901-2"""
-    out = """Albin Janusz, "Polski ruch narodowy na Łotwie w latach 1919-40", [[Wrocław]] [[1993]], ISBN 83-229-0901-2"""
-    assert strip_markup(_in) == out
-
-def strip_markup(text):
-    text = re.sub(re.compile("{{[^}}]+}}", re.M), "", text)
-    text = localesub("'''", "", text)
-    text = localesub("''", "", text)
-    text = localesub("=== (.+?) ===", r"\1", text)
-    text = localesub("== (.+?) ==", r"\1", text)
-    text = localesub("{{.+?}}", "", text)
-    text = re.sub(re.compile("^\* ", re.M), "", text)
-    return text
-
-def test_strip_wiki_markup():
-    print repr(strip_wiki_markup(corpus.gim))
-    print repr(corpus.gim_stripped)
-    assert strip_wiki_markup(corpus.gim) == corpus.gim_stripped
-
-def strip_wiki_markup(text):
-    text = strip_markup(text)
-    text = strip_info(text)
-    text = strip_links(text)
-
-    meta_data = [ ('key-word', ['first']), ("category", ['pierwsza', 'druga', 'trzecia']) ]
-
-    return meta_data, text
+from stripper import strip_wiki_markup
 
 if __name__ == "__main__":
-#    print strip_wiki_markup(corpus.poland)
     if len(sys.argv) < 2:
         print "usage: main.py test.xml"
     else:
-					# parser jest nie potrzebny
         xml2text.convert(sys.argv[1], strip_wiki_markup, True)
-	import finder
-	finder.demo()
+
+	#import finder
+	#finder.demo()
 	
