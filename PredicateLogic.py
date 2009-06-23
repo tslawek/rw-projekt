fc5a4c698f3ecf3c58a2cc0796918feb70c3feaf
tslawek <tomek.slawek@gmail.com>
2009-06-23 21:03:45 +0200

    predicate logic

---
+++ PredicateLogic.py
@@ -0,0 +1,198 @@
+#encoding=utf-8
+"""
+    n słów wczesniej [ słowo ]  n słów dalej
+    od    początek, koniec zdania, do
+    warunki co do gramatyki (clp)
+    negacja warunków
+
+    kazdy warunek (predykat) musi czegoś dotyczyć (miejsca)
+    
+        porównać do konkretnego stringa,
+        należy do zbioru z clp
+    
+        słowo w dowolnej odmianie, albo konkretny przypadek.
+"""
+_or = lambda a, b: a or b
+_and = lambda a, b: a and b
+
+class SinglePredicate(object):
+    def __or__(self, other):
+        return Or(self, other)
+    def __and__(self, other):
+        return And(self, other)
+    def __invert__(self):
+        return Not(self)
+class Not(SinglePredicate):
+    def __init__(self, pred):
+        self.pred = pred
+    def match(self, word):
+        return not self.pred.match(word)
+def LogicalPair(relation):
+    class Logical(SinglePredicate):
+        def __init__(self, pred1, pred2):
+            self.pred1, self.pred2 = pred1, pred2
+        def match(self, word):
+            return relation(self.pred1.match(word), (self.pred2.match(word)))
+    return Logical
+Or = LogicalPair(_or)
+And = LogicalPair(_and)
+
+class WordAnyForm(SinglePredicate):
+    def __init__(self, target):
+        pass
+    def match(self):
+        raise NotImplementedError()
+class FormCheck(SinglePredicate):
+    def __init__(self, form_prefix):
+        self.form_prefix = form_prefix
+    def match(self, word):
+        raise NotImplementedError()
+class StartingWith(SinglePredicate):
+    def __init__(self, prefix):
+        self.prefix = prefix
+    def match(self, word):
+        return word.startswith(self.prefix)
+class ExactWord(SinglePredicate):
+    def __init__(self, word):
+        self.word = word
+    def match(self, word):
+        return self.word == word
+
+def test_single_predicate():
+    assert ExactWord("ala").match("ala")
+    assert not ExactWord("ala").match("dala")
+    assert StartingWith("pre").match("predefiniowac")
+    assert not StartingWith("pre").match("alicja")
+
+    assert (ExactWord("ala") & ExactWord("ala")).match("ala")
+    assert not (ExactWord("ala") & ExactWord("ola")).match("ala")
+    assert (ExactWord("ala") | ExactWord("ola")).match("ala")
+    assert (ExactWord("ala") | ExactWord("ola")).match("ola")
+
+
+
+class MultiPredicate(object): pass
+class Verbatim(MultiPredicate):
+    def __init__(self, target):
+        self.target = target
+    def match_words(self, words):
+        return self.target in " ".join(words)
+class AnyWord(MultiPredicate):
+    def __init__(self, pred):
+        self.pred = pred
+    def match_words(self, words):
+        return any(self.pred.match(word) for word in words)
+def Collect(manner):
+    class Collector(MultiPredicate):
+        def __init__(self, pred):
+            self.pred = pred
+        def match_words(self, words):
+            return manner(self.pred.match(word) for word in words)
+    return Collector
+AnyWord = Collect(any)
+AllWords = Collect(all)
+
+def test_multipred():
+    words = "ala ma kota".split()
+    assert AnyWord(ExactWord("ala")).match_words(words)
+    assert not AnyWord(ExactWord("ola")).match_words(words)
+    assert AllWords(~(ExactWord("ola"))).match_words(words)
+
+
+
+
+class Scope(object): pass
+class Before(Scope):
+    def __init__(self, n):
+        self.n = n
+    def narrow(self, context):
+        range = min(len(context[0]), self.n)
+        return context[0][-range:]
+class After(Scope):
+    def __init__(self, n):
+        self.n = n
+    def narrow(self, context):
+        range = min(len(context[2]), self.n)
+        return context[2][:range]
+class Itself(Scope):
+    def narrow(self, context):
+        return [context[1]]
+ALLBEFORE = Before(float("infinity"))
+ALLAFTER = After(float("infinity"))
+ITSELF = Itself()
+
+def test_scope():
+    sentence = "a b c d e".split()
+    context = sentence[:2], sentence[2], sentence[3:]
+    assert ALLBEFORE.narrow(context) == ["a", "b"]
+    assert Before(1).narrow(context) == ["b"]
+    assert ITSELF.narrow(context) == ["c"]
+    assert After(1).narrow(context) == ["d"]
+    assert ALLAFTER.narrow(context) == ["d", "e"]
+
+
+no_nouns = AllWords(~FormCheck("A"))
+specific_word = Verbatim("brazylijska")
+specific_expression = Verbatim("ma kota")
+includes_brazilian = AnyWord(WordAnyForm("brazylijska"))
+specific_words = AnyWord(ExactWord("brazylijska") | ExactWord("kolumbijska") | ExactWord("francuska"))
+
+class Placed(object):
+    def __init__(self, scope, multipred):
+        self.scope = scope
+        self.pred = multipred
+    def match_word_in_sentence(self, words, n):
+        to_examine = self.scope.narrow((words[:n], words[n], words[n + 1:]))
+        return self.pred.match_words(to_examine)
+    def __and__(self, other):
+        return PlacedAnd(self, other)
+    def __or__(self, other):
+        return PlacedOr(self, other)
+    def __invert__(self):
+        return PlacedNot(self)
+class PlacedNot(object):
+    def __init__(self, ca):
+        self.ca = ca
+    def match_word_in_sentence(self, words, n):
+        return not self.ca.match_word_in_sentence(words, n)
+def PlacedLogical(oper):
+    class ContextAwareOperator(object):
+        def __init__(self, ca1, ca2):
+            self.ca1, self.ca2 = ca1, ca2
+        def match_word_in_sentence(self, words, n):
+            return oper(self.ca1.match_word_in_sentence(words, n), self.ca2.match_word_in_sentence(words, n))
+    return ContextAwareOperator
+PlacedAnd = PlacedLogical(_and)
+PlacedOr = PlacedLogical(_or)
+
+adj_bef_and_noun_aft       = Placed(ALLBEFORE, AnyWord(FormCheck("C"))) & Placed(ALLAFTER, AnyWord(FormCheck("A")))
+adj_bef_and_noun_immid_aft = Placed(ALLBEFORE, AnyWord(FormCheck("C"))) & Placed(After(1), AnyWord(FormCheck("A")))
+
+def test_placed():
+    words = "ala ma kota".split()
+    matcher = Placed(ALLBEFORE, AnyWord(ExactWord("ala")))
+    assert not matcher.match_word_in_sentence(words, 0)
+    assert matcher.match_word_in_sentence(words, 1)
+    assert matcher.match_word_in_sentence(words, 2)
+
+    matcher = Placed(ALLBEFORE, AnyWord(ExactWord("ala"))) & Placed(ALLAFTER, AnyWord(ExactWord("kota")))
+    assert not matcher.match_word_in_sentence(words, 0)
+    assert matcher.match_word_in_sentence(words, 1)
+
+def find(matcher, sentence):
+    words = sentence.split()
+    result = []
+    for n in range(len(words)):
+        if matcher.match_word_in_sentence(words, n):
+            result.append(words[n])
+    return result
+
+def test_finder():
+    sentence = "ala ma kota"
+    sentence2 = "ala kowalska ma wspanialego kota"
+    matcher = Placed(ALLBEFORE, AnyWord(ExactWord("ala"))) & Placed(ALLAFTER, AnyWord(ExactWord("kota")))
+    assert find(matcher, sentence) == ["ma"]
+    assert find(matcher, sentence2) == ["kowalska", "ma", "wspanialego"]
+
+    matcher = Placed(Before(2), AnyWord(ExactWord("ala"))) & Placed(After(2), AnyWord(ExactWord("kota")))
+    assert find(matcher, sentence2) == ["ma"]
--- finder.py
+++ finder.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from configuration import *
+from collections import defaultdict
 import re
 wiki_text_file_name = read_configuration('Return')['name']
 tags = read_configuration('Tags')
@@ -46,7 +47,7 @@ def read_wikipedia():
 		file.close()
 	
 	
-def find_names(text):
+def find_names_in_text(text):
 	ret = []
 	words_pattern = re.compile("\w+")
 	p = re.compile("[\w+|\s]{1,30}(?= \(ur\.)")
@@ -65,7 +66,100 @@ def find_names(text):
 	return ret
 			
 
-def demo():
+
+def context(word, text):
+	all_words = [""] + [w.lower() for w in text.split()] + [""]
+	pos = all_words.index(word)
+	word_before = all_words[pos - 1]
+	word_after = all_words[pos + 1]
+	return word_before, word_after
+
+def find_countries():
+
+	COUNTRIES = set("polska niemiecka francuska belgijska czeska".split())
+	prefixes, suffixes = defaultdict(int), defaultdict(int)
+
+	for meta, text in read_wikipedia():
+		words = set(w.lower() for w in text.split())
+		if words & COUNTRIES:
+			for word in words & COUNTRIES:
+				cb, ca = context(word, text)
+				if cb.isalpha():
+					prefixes[cb] += 1
+				if ca.isalpha():
+					suffixes[ca] += 1
+	#print 'PREFIXES:'
+	#for n, w in sorted(((v, k) for (k, v) in prefixes.items()), reverse=True)[:20]:
+		#print n, w
+	#print 'SUFFIXES:'
+	#for n, w in sorted(((v, k) for (k, v) in suffixes.items()), reverse=True)[:20]:
+		#print n, w
+
+	popular_prefixes = [word for n, word in sorted(((v, k) for (k, v) in prefixes.items()), reverse=True)[:100]]
+	popular_suffixes = [word for n, word in sorted(((v, k) for (k, v) in suffixes.items()), reverse=True)[:100]]
+	
+	other_p, other_s = defaultdict(list), defaultdict(list)
+
+	for _, text in read_wikipedia():
+		words = text.lower().split()
+		for prefix in popular_prefixes:
+			if prefix.lower() in words and words[-1] != prefix:
+				other_p[prefix].append(words[words.index(prefix) + 1])
+				if len(other_p[prefix]) > 100:
+					break
+		
+		for sufffix in popular_suffixes:
+			if sufffix.lower() in words and words[0] != sufffix:
+				other_s[sufffix].append(words[words.index(sufffix) - 1])
+				if len(other_s[sufffix]) > 100:
+					break
+
+	discriminator_p, discriminator_s = defaultdict(list), defaultdict(list)
+	for prefix, words in other_p.items():
+		print 
+#		print prefix.upper()
+		i, all = 0.0 , 1.0
+		for w in words:
+			if len(w) > 2:
+				all +=1
+				if w in COUNTRIES:
+					i +=1
+		discriminator_p[prefix] = i/all
+
+
+	for sufffix, words in other_s.items():
+		print 
+#		print sufffix.upper()
+
+
+		i, all = 0.0 , 1.0
+		for w in words:
+			if len(w) > 2:
+				all +=1
+				if w in COUNTRIES:
+					i +=1
+		discriminator_s[sufffix] = i/all
+
+	pref_ =  sorted(((v, k) for (k, v) in discriminator_p.items()), reverse=True)
+	suff_ =  sorted(((v, k) for (k, v) in discriminator_s.items()), reverse=True)
+		
+	
+	p = set()
+	s = set()
+	for i in range(10):
+		p |= set(other_p[pref_[i][1]]) 
+		s |= set(other_s[suff_[i][1]])
+	
+		#ret = set(other_p[pref_[i][1]]) & set(other_s[suff_[i][1]])
+	#	print s
+	#	print '-' * 80
+	#	print p
+
+	print '-' * 80
+	print p & s
+			
+
+def find_names():
 
 	counter = 0
 	ok =0 
@@ -73,7 +167,7 @@ def demo():
 	for i in read_wikipedia():
 		try:
 				
-			names = find_names(i[1])
+			names = find_names_in_text(i[1])
 			counter +=1
 			if len(names) > 0:
 				print names
@@ -89,6 +183,64 @@ def demo():
 			if (counter%1000) == 0:
 				
 				print "ok: %s / all: %s" %(ok, counter)
+"""
+COUNTRIES = set("polska niemiecka francuska belgijska czeska".split())
+
+
+def demo2():
+	prefixes, suffixes = defaultdict(int), defaultdict(int)
+
+	for meta, text in read_wikipedia():
+		words = set(w.lower() for w in text.split())
+		if words & COUNTRIES:
+			for word in words & COUNTRIES:
+				cb, ca = context(word, text)
+				if cb.isalpha():
+					prefixes[cb] += 1
+				if ca.isalpha():
+					suffixes[ca] += 1
+	#print 'PREFIXES:'
+	#for n, w in sorted(((v, k) for (k, v) in prefixes.items()), reverse=True)[:20]:
+		#print n, w
+	#print 'SUFFIXES:'
+	#for n, w in sorted(((v, k) for (k, v) in suffixes.items()), reverse=True)[:20]:
+		#print n, w
+
+	popular_prefixes = [word for n, word in sorted(((v, k) for (k, v) in prefixes.items()), reverse=True)[:100]]
+	popular_suffixes = [word for n, word in sorted(((v, k) for (k, v) in suffixes.items()), reverse=True)[:100]]
+	
+	other_p, other_s = defaultdict(list), defaultdict(list)
+
+	for _, text in read_wikipedia():
+		words = text.lower().split()
+		for prefix in popular_prefixes:
+			if prefix.lower() in words and words[-1] != prefix:
+				other_p[prefix].append(words[words.index(prefix) + 1])
+				if len(other_p[prefix]) > 100:
+					break
+		
+		for sufffix in popular_suffixes:
+			if sufffix.lower() in words and words[0] != sufffix:
+				other_s[sufffix].append(words[words.index(sufffix) - 1])
+				if len(other_s[sufffix]) > 100:
+					break
+
+	for prefix, words in other_p.items():
+		print 
+		print prefix.upper()
+		print "  ".join(words)
+
+	for sufffix, words in other_s.items():
+		print 
+		print sufffix.upper()
+		print "  ".join(words)
+		
+"""
+
+
+				
+
 
 if __name__ == '__main__':
-	demo()
+	find_countries()
+	find_names()
